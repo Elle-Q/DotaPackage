ee3a61d7fb6a82384c2cd3f1bd65f1ae2359d46a
elle <5268568+ellecommander@user.noreply.gitee.com>
2019-12-24 22:54:31 +0800

    解析txt文件工具类、生成json文件

---
+++ src/main/java/utils/FileUtill.java
@@ -0,0 +1,66 @@
+package utils;
+
+import com.google.gson.*;
+import com.intellij.icons.AllIcons;
+
+import javax.sound.midi.SoundbankResource;
+import java.io.*;
+
+/**
+ * 处理文件的工具类，用来解析恶心的txt文件
+ * User: pretty el
+ * Date: 2019/12/24
+ */
+public class FileUtill {
+
+
+    public static void main(String[] args) {
+        try {
+            FileReader file = new FileReader("E:\\GradelDemos\\DotaPackage\\src\\main\\resources\\docs\\cl_panorama_script_help_2.txt");
+            BufferedReader br = new BufferedReader(file);
+            int lineNum = 0;//文件行数
+            String line = "";
+            boolean flag = false;
+            JsonObject funcJson = new JsonObject();
+            JsonArray array = new JsonArray();
+            while (null != (line = br.readLine())) {
+                boolean isSave = false;
+                if (flag) {
+                    flag = false;
+                    isSave = true;
+                    funcJson.addProperty("Description", line.replace("|", "").trim());
+                }
+                if (line.contains("<code>")) {
+                    funcJson = new JsonObject();
+                    flag = true;
+                    funcJson.addProperty("Signature", line.replace("<code>", "")
+                            .replace("</code>", "").replace("|", "").trim());
+                }
+                if (isSave && funcJson.size() > 1) {
+                    array.add(funcJson);
+//                    System.out.println(funcJson.toString());
+                }
+            }
+            System.out.println("共创建对象：" + array.size() + "个");
+            System.out.println(array.toString());
+            //生成json格式文件
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonParser jp = new JsonParser();
+            JsonElement je = jp.parse(array.toString());
+            String prettyJoson = gson.toJson(je);
+            File jsonFile = new File("E:\\\\GradelDemos\\\\DotaPackage\\\\src\\\\main\\\\resources\\\\docs\\\\js_funcs.json");
+            if (!jsonFile.getParentFile().exists()) {
+                jsonFile.mkdirs();
+            }
+            if (jsonFile.exists()) {
+                jsonFile.delete();
+            }
+            jsonFile.createNewFile();
+            //将格式化后的字符串写入文件
+            Writer writer = new OutputStreamWriter(new FileOutputStream(jsonFile),"utf-8");
+            gson.toJson(je, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
